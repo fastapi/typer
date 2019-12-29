345446321efcd1edfe788c68c677d2f86587c4fa
Sebastián Ramírez <tiangolo@gmail.com>
2019-12-29 10:23:31 +0100

    :memo: Add CLI Options docs and examples

---
+++ docs/src/options/tutorial004.py
@@ -0,0 +1,12 @@
+import typer
+
+
+def main(
+    name: str,
+    lastname: str = typer.Option(..., prompt="Please tell me your last name"),
+):
+    typer.echo(f"Hello {name} {lastname}")
+
+
+if __name__ == "__main__":
+    typer.run(main)
