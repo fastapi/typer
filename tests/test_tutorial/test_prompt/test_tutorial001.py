7673cab540dc643c59504b08d96f4b21a7aad6be
Sebastián Ramírez <tiangolo@gmail.com>
2020-01-02 16:59:26 +0100

    :white_check_mark: Add tests for prompts

---
+++ tests/test_tutorial/test_prompt/test_tutorial001.py
@@ -0,0 +1,28 @@
+import subprocess
+
+import typer
+from typer.testing import CliRunner
+
+from prompt import tutorial001 as mod
+
+runner = CliRunner()
+
+app = typer.Typer()
+app.command()(mod.main)
+
+
+def test_cli():
+    result = runner.invoke(app, input="Camila\n")
+    assert result.exit_code == 0
+    assert "What's your name?:" in result.output
+    assert "Hello Camila" in result.output
+
+
+def test_script():
+    result = subprocess.run(
+        ["coverage", "run", mod.__file__, "--help"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    assert "Usage" in result.stdout
---
+++ tests/test_tutorial/test_prompt/test_tutorial003.py
@@ -0,0 +1,35 @@
+import subprocess
+
+import typer
+from typer.testing import CliRunner
+
+from prompt import tutorial003 as mod
+
+runner = CliRunner()
+
+app = typer.Typer()
+app.command()(mod.main)
+
+
+def test_cli():
+    result = runner.invoke(app, input="y\n")
+    assert result.exit_code == 0
+    assert "Are you sure you want to delete it? [y/N]:" in result.output
+    assert "Deleting it!" in result.output
+
+
+def test_no_confirm():
+    result = runner.invoke(app, input="n\n")
+    assert result.exit_code == 1
+    assert "Are you sure you want to delete it? [y/N]:" in result.output
+    assert "Aborted!" in result.output
+
+
+def test_script():
+    result = subprocess.run(
+        ["coverage", "run", mod.__file__, "--help"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    assert "Usage" in result.stdout
