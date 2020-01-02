fb46cc056cfbfbc97fd099ef856b28a7786f8145
Sebastián Ramírez <tiangolo@gmail.com>
2020-01-02 18:42:36 +0100

    :memo: Add docs for get_app_dir()

---
+++ docs/src/app_dir/tutorial001.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import typer
+
+APP_NAME = "my-super-cli-app"
+
+
+def main():
+    app_dir = typer.get_app_dir(APP_NAME)
+    config_path = Path(app_dir) / "config.json"
+    if not config_path.is_file():
+        typer.echo("Config file doesn't exist yet")
+
+
+if __name__ == "__main__":
+    typer.run(main)
