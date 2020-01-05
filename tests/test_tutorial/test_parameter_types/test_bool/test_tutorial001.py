be75c9c3a36ae6c3912a98e45b1ca9bc69689483
Sebastián Ramírez <tiangolo@gmail.com>
2020-01-05 21:53:57 +0100

    :white_check_mark: Add tests for extra param types, bool, number

---
+++ tests/test_tutorial/test_parameter_types/test_bool/test_tutorial001.py
@@ -0,0 +1,46 @@
+import subprocess
+
+import typer
+from typer.testing import CliRunner
+
+from parameter_types.bool import tutorial001 as mod
+
+runner = CliRunner()
+
+app = typer.Typer()
+app.command()(mod.main)
+
+
+def test_help():
+    result = runner.invoke(app, ["--help"])
+    assert result.exit_code == 0
+    assert "--force" in result.output
+    assert "--no-force" not in result.output
+
+
+def test_no_force():
+    result = runner.invoke(app)
+    assert result.exit_code == 0
+    assert "Not forcing" in result.output
+
+
+def test_force():
+    result = runner.invoke(app, ["--force"])
+    assert result.exit_code == 0
+    assert "Forcing operation" in result.output
+
+
+def test_invalid_no_force():
+    result = runner.invoke(app, ["--no-force"])
+    assert result.exit_code != 0
+    assert "Error: no such option: --no-force" in result.output
+
+
+def test_script():
+    result = subprocess.run(
+        ["coverage", "run", mod.__file__, "--help"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    assert "Usage" in result.stdout
---
+++ tests/test_tutorial/test_parameter_types/test_bool/test_tutorial003.py
@@ -0,0 +1,39 @@
+import subprocess
+
+import typer
+from typer.testing import CliRunner
+
+from parameter_types.bool import tutorial003 as mod
+
+runner = CliRunner()
+
+app = typer.Typer()
+app.command()(mod.main)
+
+
+def test_help():
+    result = runner.invoke(app, ["--help"])
+    assert result.exit_code == 0
+    assert "-f, --force / -F, --no-force" in result.output
+
+
+def test_force():
+    result = runner.invoke(app, ["-f"])
+    assert result.exit_code == 0
+    assert "Forcing operation" in result.output
+
+
+def test_no_force():
+    result = runner.invoke(app, ["-F"])
+    assert result.exit_code == 0
+    assert "Not forcing" in result.output
+
+
+def test_script():
+    result = subprocess.run(
+        ["coverage", "run", mod.__file__, "--help"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    assert "Usage" in result.stdout
