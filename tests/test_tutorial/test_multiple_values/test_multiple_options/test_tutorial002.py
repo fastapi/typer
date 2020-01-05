724c4bc8b63d5d6088247ab141ce58ac70243a42
Sebastián Ramírez <tiangolo@gmail.com>
2020-01-05 22:10:54 +0100

    :white_check_mark: Add tests for examples with multiple values

---
+++ tests/test_tutorial/test_multiple_values/test_multiple_options/test_tutorial002.py
@@ -0,0 +1,38 @@
+import subprocess
+
+import typer
+from typer.testing import CliRunner
+
+from multiple_values.multiple_options import tutorial002 as mod
+
+runner = CliRunner()
+app = typer.Typer()
+app.command()(mod.main)
+
+
+def test_main():
+    result = runner.invoke(app)
+    assert result.exit_code == 0
+    assert "The sum is 0" in result.output
+
+
+def test_1_number():
+    result = runner.invoke(app, ["--number", "2"])
+    assert result.exit_code == 0
+    assert "The sum is 2.0" in result.output
+
+
+def test_2_number():
+    result = runner.invoke(app, ["--number", "2", "--number", "3", "--number", "4.5"])
+    assert result.exit_code == 0
+    assert "The sum is 9.5" in result.output
+
+
+def test_script():
+    result = subprocess.run(
+        ["coverage", "run", mod.__file__, "--help"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    assert "Usage" in result.stdout
