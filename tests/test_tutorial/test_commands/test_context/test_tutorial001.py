18a26e2a7275710c7ca1ed502a1d4076f71a565c
Sebastián Ramírez <tiangolo@gmail.com>
2020-03-18 16:58:41 +0100

    :white_check_mark: Add tests for using the context

---
+++ tests/test_tutorial/test_commands/test_context/test_tutorial001.py
@@ -0,0 +1,32 @@
+import subprocess
+from commands.context import tutorial001 as mod
+
+from typer.testing import CliRunner
+
+app = mod.app
+
+runner = CliRunner()
+
+
+def test_create():
+    result = runner.invoke(app, ["create", "Camila"])
+    assert result.exit_code == 0
+    assert "About to execute command: create" in result.output
+    assert "Creating user: Camila" in result.output
+
+
+def test_delete():
+    result = runner.invoke(app, ["delete", "Camila"])
+    assert result.exit_code == 0
+    assert "About to execute command: delete" in result.output
+    assert "Deleting user: Camila" in result.output
+
+
+def test_script():
+    result = subprocess.run(
+        ["coverage", "run", mod.__file__, "--help"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    assert "Usage" in result.stdout
