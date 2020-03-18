fc5c16812a9117bc4291ad7e874059380409fae5
Sebastián Ramírez <tiangolo@gmail.com>
2020-03-18 16:58:08 +0100

    :sparkles: Document using the context

---
+++ docs/src/commands/context/tutorial004.py
@@ -0,0 +1,15 @@
+import typer
+
+app = typer.Typer()
+
+
+@app.command(
+    context_settings={"allow_extra_args": True, "ignore_unknown_options": True}
+)
+def main(ctx: typer.Context):
+    for extra_arg in ctx.args:
+        typer.echo(f"Got extra arg: {extra_arg}")
+
+
+if __name__ == "__main__":
+    app()
