8c70745ab775cd445344ded9dea62bec86727a94
Sebastián Ramírez <tiangolo@gmail.com>
2020-04-26 11:59:44 +0200

    :sparkles: Re-implement completion install to lazy load
    to support completion for programs under environments

--- typer/completion.py
+++ typer/completion.py
@@ -39,8 +39,8 @@ def install_callback(ctx: click.Context, param: click.Parameter, value: Any) -> 
         shell, path = install(shell=value)
     else:
         shell, path = install()
-    click.secho(f"{shell} completion installed in {path}.", fg="green")
-    click.echo("Completion will take effect once you restart the terminal.")
+    click.secho(f"{shell} completion installed in {path}", fg="green")
+    click.echo("Completion will take effect once you restart the terminal")
     sys.exit(0)
 
 
@@ -54,7 +54,9 @@ def show_callback(ctx: click.Context, param: click.Parameter, value: Any) -> Any
         shell = value
     elif shellingham:
         shell, _ = shellingham.detect_shell()
-    script_content = get_completion_script(prog_name, complete_var, shell)
+    script_content = get_completion_script(
+        prog_name=prog_name, complete_var=complete_var, shell=shell
+    )
     click.echo(script_content)
     sys.exit(0)
 
@@ -164,71 +166,137 @@ def install(
         complete_var = "_{}_COMPLETE".format(prog_name.replace("-", "_").upper())
     if shell is None and shellingham is not None:
         shell, _ = shellingham.detect_shell()
-    mode = None
     if shell == "bash":
-        path_obj = Path.home() / ".bashrc"
-        mode = mode or "a"
+        installed_path = install_bash(
+            prog_name=prog_name, complete_var=complete_var, shell=shell
+        )
+        return shell, installed_path
     elif shell == "zsh":
-        path_obj = Path.home() / ".zshrc"
-        mode = mode or "a"
+        installed_path = install_zsh(
+            prog_name=prog_name, complete_var=complete_var, shell=shell
+        )
+        return shell, installed_path
     elif shell == "fish":
-        path_obj = Path.home() / f".config/fish/completions/{prog_name}.fish"
-        mode = mode or "w"
+        installed_path = install_fish(
+            prog_name=prog_name, complete_var=complete_var, shell=shell
+        )
+        return shell, installed_path
     elif shell in {"powershell", "pwsh"}:
-        subprocess.run(
-            [
-                shell,
-                "-Command",
-                "Set-ExecutionPolicy",
-                "Unrestricted",
-                "-Scope",
-                "CurrentUser",
-            ]
+        installed_path = install_powershell(
+            prog_name=prog_name, complete_var=complete_var, shell=shell
         )
-        result = subprocess.run(
-            [shell, "-NoProfile", "-Command", "echo", "$profile"],
-            check=True,
-            stdout=subprocess.PIPE,
-        )
-        if result.returncode != 0:  # pragma: nocover
-            click.echo("Couldn't get PowerShell user profile", err=True)
-            raise click.exceptions.Exit(result.returncode)
-        path_str = ""
-        if isinstance(result.stdout, str):  # pragma: nocover
-            path_str = result.stdout
-        if isinstance(result.stdout, bytes):
-            try:
-                # PowerShell would be predominant in Windows
-                path_str = result.stdout.decode("windows-1252")
-            except UnicodeDecodeError:  # pragma: nocover
-                try:
-                    path_str = result.stdout.decode("utf8")
-                except UnicodeDecodeError:
-                    click.echo("Couldn't decode the path automatically", err=True)
-                    raise click.exceptions.Exit(1)
-        path_obj = Path(path_str.strip())
-        mode = mode or "a"
+        return shell, installed_path
     else:
         click.echo(f"Shell {shell} is not supported.")
         raise click.exceptions.Exit(1)
+
+
+def install_bash(*, prog_name: str, complete_var: str, shell: str) -> Path:
+    # Ref: https://github.com/scop/bash-completion#faq
+    # It seems bash-completion is the official completion system for bash:
+    # Ref: https://www.gnu.org/software/bash/manual/html_node/A-Programmable-Completion-Example.html
+    # But installing in the locations from the docs doesn't seem to have effect
+    completion_path = Path.home() / f".bash_completions/{prog_name}.sh"
+    rc_path = Path.home() / ".bashrc"
+    rc_path.parent.mkdir(parents=True, exist_ok=True)
+    rc_content = ""
+    if rc_path.is_file():
+        rc_content = rc_path.read_text()
+    completion_init_lines = [f"source {completion_path}"]
+    for line in completion_init_lines:
+        if line not in rc_content:
+            rc_content += f"\n{line}"
+    rc_content += "\n"
+    rc_path.write_text(rc_content)
+    # Install completion
+    completion_path.parent.mkdir(parents=True, exist_ok=True)
+    script_content = get_completion_script(
+        prog_name=prog_name, complete_var=complete_var, shell=shell
+    )
+    completion_path.write_text(script_content)
+    return completion_path
+
+
+def install_zsh(*, prog_name: str, complete_var: str, shell: str) -> Path:
+    # Setup Zsh and load ~/.zfunc
+    zshrc_path = Path.home() / ".zshrc"
+    zshrc_path.parent.mkdir(parents=True, exist_ok=True)
+    zshrc_content = ""
+    if zshrc_path.is_file():
+        zshrc_content = zshrc_path.read_text()
+    completion_init_lines = [
+        "autoload -Uz compinit",
+        "compinit",
+        "zstyle ':completion:*' menu select",
+        "fpath+=~/.zfunc",
+    ]
+    for line in completion_init_lines:
+        if line not in zshrc_content:
+            zshrc_content += f"\n{line}"
+    zshrc_content += "\n"
+    zshrc_path.write_text(zshrc_content)
+    # Install completion under ~/.zfunc/
+    path_obj = Path.home() / f".zfunc/_{prog_name}"
+    path_obj.parent.mkdir(parents=True, exist_ok=True)
+    script_content = get_completion_script(
+        prog_name=prog_name, complete_var=complete_var, shell=shell
+    )
+    path_obj.write_text(script_content)
+    return path_obj
+
+
+def install_fish(*, prog_name: str, complete_var: str, shell: str) -> Path:
+    path_obj = Path.home() / f".config/fish/completions/{prog_name}.fish"
     parent_dir: Path = path_obj.parent
     parent_dir.mkdir(parents=True, exist_ok=True)
-    script_content = get_installable_script(prog_name, complete_var, shell)
-    with path_obj.open(mode=mode) as f:
+    script_content = get_completion_script(
+        prog_name=prog_name, complete_var=complete_var, shell=shell
+    )
+    path_obj.write_text(f"{script_content}\n")
+    return path_obj
+
+
+def install_powershell(*, prog_name: str, complete_var: str, shell: str) -> Path:
+    subprocess.run(
+        [
+            shell,
+            "-Command",
+            "Set-ExecutionPolicy",
+            "Unrestricted",
+            "-Scope",
+            "CurrentUser",
+        ]
+    )
+    result = subprocess.run(
+        [shell, "-NoProfile", "-Command", "echo", "$profile"],
+        check=True,
+        stdout=subprocess.PIPE,
+    )
+    if result.returncode != 0:  # pragma: nocover
+        click.echo("Couldn't get PowerShell user profile", err=True)
+        raise click.exceptions.Exit(result.returncode)
+    path_str = ""
+    if isinstance(result.stdout, str):  # pragma: nocover
+        path_str = result.stdout
+    if isinstance(result.stdout, bytes):
+        try:
+            # PowerShell would be predominant in Windows
+            path_str = result.stdout.decode("windows-1252")
+        except UnicodeDecodeError:  # pragma: nocover
+            try:
+                path_str = result.stdout.decode("utf8")
+            except UnicodeDecodeError:
+                click.echo("Couldn't decode the path automatically", err=True)
+                raise click.exceptions.Exit(1)
+    path_obj = Path(path_str.strip())
+    parent_dir: Path = path_obj.parent
+    parent_dir.mkdir(parents=True, exist_ok=True)
+    script_content = get_completion_script(
+        prog_name=prog_name, complete_var=complete_var, shell=shell
+    )
+    with path_obj.open(mode="a") as f:
         f.write(f"{script_content}\n")
-    return shell, path_obj
-
-
-def get_installable_script(prog_name: str, complete_var: str, shell: str) -> str:
-    if shell == "bash":
-        return f'eval "$({complete_var}=source_bash {prog_name} 2>/dev/null)"'
-    elif shell == "zsh":
-        return f'eval "$({complete_var}=source_zsh {prog_name} 2>/dev/null)"'
-    elif shell == "fish":
-        return f"eval (env {complete_var}=source_fish {prog_name} 2>/dev/null)"
-    elif shell in {"powershell", "pwsh"}:
-        return get_completion_script(prog_name, complete_var, shell)
-    return ""
+    return path_obj
 
 
 def do_bash_complete(cli: click.Command, prog_name: str) -> bool:
@@ -340,7 +408,7 @@ _completion_scripts = {
 }
 
 
-def get_completion_script(prog_name: str, complete_var: str, shell: str) -> str:
+def get_completion_script(*, prog_name: str, complete_var: str, shell: str) -> str:
     cf_name = click._bashcomplete._invalid_ident_char_re.sub(
         "", prog_name.replace("-", "_")
     )
@@ -366,10 +434,11 @@ def handle_shell_complete(
         sys.exit(1)
     command, shell = complete_instr.split("_", 1)
     if command == "source":
-        click.echo(get_completion_script(prog_name, complete_var, shell))
-        return True
-    elif command == "install-source":
-        click.echo(get_installable_script(prog_name, complete_var, shell))
+        click.echo(
+            get_completion_script(
+                prog_name=prog_name, complete_var=complete_var, shell=shell
+            )
+        )
         return True
     elif command == "complete":
         return do_shell_complete(cli=cli, prog_name=prog_name, shell=shell)
