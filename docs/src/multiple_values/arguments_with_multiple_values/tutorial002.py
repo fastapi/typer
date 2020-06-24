2eec42cf138d3f4409cd5967d420d8260f7e907c
Sebastián Ramírez <tiangolo@gmail.com>
2020-06-24 19:30:48 +0200

    📝 Add example of CLI arguments with tuples

---
+++ docs/src/multiple_values/arguments_with_multiple_values/tutorial002.py
@@ -0,0 +1,16 @@
+from typing import Tuple
+
+import typer
+
+
+def main(
+    names: Tuple[str, str, str] = typer.Argument(
+        ("Harry", "Hermione", "Ron"), help="Select 3 characters to play with"
+    )
+):
+    for name in names:
+        typer.echo(f"Hello {name}")
+
+
+if __name__ == "__main__":
+    typer.run(main)
