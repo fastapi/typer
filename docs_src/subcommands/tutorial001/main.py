1dbe774fc30d47000294f71ccd0fbbac74325efd
Sebastián Ramírez <tiangolo@gmail.com>
2020-07-09 21:11:23 +0200

    🚚 Move docs src files to root docs_src

--- docs_src/subcommands/tutorial001/main.py
+++ docs_src/subcommands/tutorial001/main.py
@@ -1,13 +1,10 @@
-import typer
-
 import items
-import lands
+import typer
 import users
 
 app = typer.Typer()
 app.add_typer(users.app, name="users")
 app.add_typer(items.app, name="items")
-app.add_typer(lands.app, name="lands")
 
 if __name__ == "__main__":
     app()
--- docs_src/subcommands/tutorial003/main.py
+++ docs_src/subcommands/tutorial003/main.py
@@ -1,11 +1,12 @@
-import typer
-
 import items
+import lands
+import typer
 import users
 
 app = typer.Typer()
 app.add_typer(users.app, name="users")
 app.add_typer(items.app, name="items")
+app.add_typer(lands.app, name="lands")
 
 if __name__ == "__main__":
     app()
