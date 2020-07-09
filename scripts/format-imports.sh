9f1fc7c9a9a9208c04b48bbae0db5a99aae8d759
Sebastián Ramírez <tiangolo@gmail.com>
2020-07-09 21:42:41 +0200

    ⬆️ Upgrade isort and configs

--- scripts/format-imports.sh
+++ scripts/format-imports.sh
@@ -2,5 +2,5 @@
 set -x
 
 # Sort imports one per line, so autoflake can remove unused imports
-isort --recursive  --force-single-line-imports --thirdparty typer --apply typer tests docs/src
+isort --force-single-line-imports typer tests docs_src
 sh ./scripts/format.sh
