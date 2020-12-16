28a7799a9503d219594c255aa2aeafeeef9698b0
Félix Voituret <Faylixe@users.noreply.github.com>
2020-12-16 12:19:46 +0100

    ✨ add custom logger handler
    The proposed logger handler use `echo` function as log record channel allowing to use traditional logging module with Typer

--- typer/utils.py
+++ typer/utils.py
@@ -1,9 +1,18 @@
 import inspect
+import logging
+
 from typing import Any, Callable, Dict, get_type_hints
 
 from .models import ParamMeta
 
 
+class TyperLoggerHandler(logging.Handler):
+    """ A custom logger handler that use Typer echo. """
+
+    def emit(self, record: logging.LogRecord) -> None:
+        echo(self.format(record))
+
+        
 def get_params_from_function(func: Callable[..., Any]) -> Dict[str, ParamMeta]:
     signature = inspect.signature(func)
     type_hints = get_type_hints(func)
