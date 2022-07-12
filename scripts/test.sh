0481647092b16290577288a51721e1269ed13f43
Sebastián Ramírez <tiangolo@gmail.com>
2022-07-12 15:58:50 +0200

    ✅ Config terminal size for tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,8 @@
 set -e
 set -x
 
+# For tests, a large terminal width
+export TERMINAL_WIDTH=3000
 bash ./scripts/test-files.sh
 # Use xdist-pytest --forked to ensure modified sys.path to import relative modules in examples keeps working
 pytest --cov=typer --cov=tests --cov=docs_src --cov-report=term-missing --cov-report=xml -o console_output_style=progress --forked --numprocesses=auto ${@}
