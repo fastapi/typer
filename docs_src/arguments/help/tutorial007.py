4100b934ba6f15d75fc54330e98116eeac05e04a
Sebastián Ramírez <tiangolo@gmail.com>
2022-07-12 11:05:18 +0200

    ✨ Add example sources for rich_help_panel

--- docs_src/arguments/help/tutorial007.py
+++ docs_src/arguments/help/tutorial007.py
@@ -1,7 +1,15 @@
 import typer
 
 
-def main(name: str = typer.Argument("World", hidden=True)):
+def main(
+    name: str = typer.Argument(..., help="Who to greet"),
+    lastname: str = typer.Argument(
+        "", help="The last name", rich_help_panel="Secondary Arguments"
+    ),
+    age: str = typer.Argument(
+        "", help="The user's age", rich_help_panel="Secondary Arguments"
+    ),
+):
     """
     Say hi to NAME very gently, like Dirk.
     """
