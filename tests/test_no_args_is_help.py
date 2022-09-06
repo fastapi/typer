a2f2df2759feae4f8b4a0cd40adcf67650cbe1ee
Jacob Bush <jacob.bush@shopify.com>
2022-09-06 19:41:44 -0400

    fix test coverage

--- tests/test_no_args_is_help.py
+++ tests/test_no_args_is_help.py
@@ -12,21 +12,13 @@ def test__no_args_is_help__and_no_commands__run_with_no_args__prints_help():
 
 def test__no_args_is_help__and_one_command__run_with_no_args__prints_help():
     app = typer.Typer(no_args_is_help=True)
-
-    @app.command()
-    def _():
-        ...
-
+    app.command("cmd")(lambda: print("Command Executed"))
     result = runner.invoke(app, [])
     assert "--help Show this message and exit." in " ".join(result.stdout.split())
 
 
 def test__no_args_is_help__and_one_command__run_with_command__executes_command():
     app = typer.Typer(no_args_is_help=True)
-
-    @app.command()
-    def _():
-        print("SUCCESS")
-
-    result = runner.invoke(app, ["-"])
-    assert result.stdout == "SUCCESS\n"
+    app.command("cmd")(lambda: print("Command Executed"))
+    result = runner.invoke(app, ["cmd"])
+    assert result.stdout == "Command Executed\n"
