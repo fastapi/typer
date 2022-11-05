db45b81f0f3073046448211a2bc54e370c02b052
Sebastián Ramírez <tiangolo@gmail.com>
2022-11-05 15:17:14 +0100

    🔧 Try to simplify pytest-cov configs

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,7 +8,7 @@ export TERMINAL_WIDTH=3000
 # Force disable terminal for tests inside of pytest, takes precedence over GITHUB_ACTIONS env var
 export _TYPER_FORCE_DISABLE_TERMINAL=1
 bash ./scripts/test-files.sh
-# Use xdist-pytest --forked to ensure modified sys.path to import relative modules in examples keeps working
-# pytest --cov-config=.coveragerc --cov --cov-report=term-missing -o console_output_style=progress --numprocesses=auto ${@}
+# It seems xdist-pytest ensures modified sys.path to import relative modules in examples keeps working
+pytest --cov-config=.coveragerc --cov --cov-report=term-missing -o console_output_style=progress --numprocesses=auto ${@}
 # coverage run -m pytest -o console_output_style=progress ${@}
-pytest --cov=typer --cov=tests --cov=docs_src --cov-report=term-missing -o console_output_style=progress --numprocesses=auto ${@}
+# pytest --cov=typer --cov=tests --cov=docs_src --cov-report=term-missing -o console_output_style=progress --numprocesses=auto ${@}
