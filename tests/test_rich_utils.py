41e055fe89c1c165d8300178569a04a78fccb5df
feluelle <felix.uellendall@astronomer.io>
2022-11-21 17:50:54 +0100

    Add test

--- tests/test_rich_utils.py
+++ tests/test_rich_utils.py
@@ -1,5 +1,9 @@
+import importlib
+
+import pytest
 import typer
 import typer.completion
+from typer.rich_utils import print
 from typer.testing import CliRunner
 
 runner = CliRunner()
@@ -36,3 +40,28 @@ def test_rich_utils_click_rewrapp():
     assert "Hello World" in result.stdout
     result = runner.invoke(app, ["secondary"])
     assert "Hello Secondary World" in result.stdout
+
+
+@pytest.mark.parametrize(
+    "terminal_width,stdout",
+    [
+        ("5", "Hello\nWorld"),
+        ("20", "Hello World"),
+    ],
+    ids=[
+        "terminal-small",
+        "terminal-large",
+    ],
+)
+def test_print(terminal_width, stdout):
+    app = typer.Typer(rich_markup_mode="rich")
+
+    @app.command()
+    def main():
+        # Reloading the module as the TERMINAL_WIDTH env var is only being loaded once.
+        importlib.reload(typer.rich_utils)
+
+        print("Hello World")
+
+    result = runner.invoke(app, env={"TERMINAL_WIDTH": terminal_width})
+    assert stdout in result.stdout
