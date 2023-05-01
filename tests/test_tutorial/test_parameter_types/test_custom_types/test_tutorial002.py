617a7259369c20bda2eaaa1ff2d4396e5f2d4af0
Sebastián Ramírez <tiangolo@gmail.com>
2023-05-01 11:28:26 +0200

    ✅ Tweak tests to run scripts and remove pragma: nocover in examples

--- tests/test_tutorial/test_parameter_types/test_custom_types/test_tutorial002.py
+++ tests/test_tutorial/test_parameter_types/test_custom_types/test_tutorial002.py
@@ -1,3 +1,6 @@
+import subprocess
+import sys
+
 import typer
 from typer.testing import CliRunner
 
@@ -24,3 +27,13 @@ def test_parse_custom_type_with_default():
     result = runner.invoke(app, ["0"])
     assert "custom_arg is <CustomClass: value=000>" in result.output
     assert "custom-opt is <CustomClass: value=YYY>" in result.output
+
+
+def test_script():
+    result = subprocess.run(
+        [sys.executable, "-m", "coverage", "run", mod.__file__, "--help"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    assert "Usage" in result.stdout
