f2e14aa0a22bbdaf0790a051d1cbe446100c617a
svlandeg <svlandeg@github.com>
2024-03-06 18:37:32 +0100

    Add test_script

--- tests/test_tutorial/test_parameter_types/test_enum/test_tutorial003.py
+++ tests/test_tutorial/test_parameter_types/test_enum/test_tutorial003.py
@@ -1,3 +1,6 @@
+import subprocess
+import sys
+
 import pytest
 import typer
 from typer.testing import CliRunner
@@ -15,3 +18,13 @@ def test_case(interval):
     result = runner.invoke(app, ["--interval", interval])
     assert result.exit_code == 0
     assert f"Found interval: {interval}" in result.output
+
+
+def test_script():
+    result = subprocess.run(
+        [sys.executable, "-m", "coverage", "run", mod.__file__, "--help"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    assert "Usage" in result.stdout
