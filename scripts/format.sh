e0b062c563aec52312b88033e414d07848c646d2
Sebastián Ramírez <tiangolo@gmail.com>
2024-03-26 12:31:09 -0500

    🔨 Update format script with Ruff

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -e
 set -x
 
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place docs_src typer tests --exclude=__init__.py
-black typer tests docs_src
-isort typer tests docs_src
+ruff typer tests docs_src scripts --fix
+ruff format typer tests docs_src scripts
