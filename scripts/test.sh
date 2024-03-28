0cdc31e6cc75aecbace7cb7dc6a28b66899d0326
Sebastián Ramírez <tiangolo@gmail.com>
2024-03-28 18:10:37 -0500

    🔧 Explicitly include testing files in sdist for redistributors (e.g. OpenSUSE) and add CI to test redistribution (#773)

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,6 +7,5 @@ set -x
 export TERMINAL_WIDTH=3000
 # Force disable terminal for tests inside of pytest, takes precedence over GITHUB_ACTIONS env var
 export _TYPER_FORCE_DISABLE_TERMINAL=1
-bash ./scripts/test-files.sh
 # It seems xdist-pytest ensures modified sys.path to import relative modules in examples keeps working
 pytest --cov --cov-report=term-missing -o console_output_style=progress --numprocesses=auto ${@}
