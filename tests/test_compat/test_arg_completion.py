ee3744a79aabf290841d727a46f4ba659ff185c2
Brian Kohan <bckohan@gmail.com>
2024-04-03 20:33:30 -0700

    fix arg completion test ruff linting issue

--- tests/test_compat/test_arg_completion.py
+++ tests/test_compat/test_arg_completion.py
@@ -8,8 +8,7 @@ from tests.assets import compat_arg_complete_click7_8 as mod
 def test_arg_completion():
     result = subprocess.run(
         [sys.executable, "-m", "coverage", "run", mod.__file__, "E"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
+        capture_output=True,
         encoding="utf-8",
         env={
             **os.environ,
