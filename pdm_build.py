76639e7e4f6a5fe03a258f9934cf7f6c4eb2e69b
Sebastián Ramírez <tiangolo@gmail.com>
2024-04-04 16:53:10 -0500

    🔨 Update pdm_build.py script to make it more generic, reading from pyproject.toml configs

--- pdm_build.py
+++ pdm_build.py
@@ -1,34 +1,36 @@
 import os
-from typing import List
+from typing import Any, Dict, List
 
 from pdm.backend.hooks import Context
 
-packages_to_sync = ["typer-slim", "typer-cli"]
-
-TYPER_SLIM_BUILD = os.getenv("TYPER_SLIM_BUILD")
-
-
-def pdm_build_hook_enabled(context: Context):
-    return context.target == "sdist"
+TIANGOLO_BUILD_PACKAGE = os.getenv("TIANGOLO_BUILD_PACKAGE", "typer")
 
 
 def pdm_build_initialize(context: Context):
     metadata = context.config.metadata
     # Get main version
     version = metadata["version"]
-    if TYPER_SLIM_BUILD:
-        metadata["name"] = "typer-slim"
-    else:
-        typer_config_data = context.config.data["tool"]["typer"]["_internal_build"]
-        typer_dependencies: List[str] = typer_config_data["dependencies"]
-
-        new_dependencies = []
-        for dep in typer_dependencies:
-            if any(dep.startswith(name) for name in packages_to_sync):
-                new_dep = f"{dep}=={version}"
-                new_dependencies.append(new_dep)
-            else:
-                new_dependencies.append(dep)
-        if new_dependencies != typer_dependencies:
-            metadata["dependencies"] = new_dependencies
-        metadata["optional-dependencies"] = {}
+    sync_dependencies: List[str] = context.config.data["tool"]["tiangolo"][
+        "_internal_slim_build"
+    ]["sync_dependencies"]
+    config: Dict[str, Any] = context.config.data["tool"]["tiangolo"][
+        "_internal_slim_build"
+    ]["packages"][TIANGOLO_BUILD_PACKAGE]
+    project_config: Dict[str, Any] = config["project"]
+    for key, value in project_config.items():
+        metadata[key] = value
+    build_config: Dict[str, Any] = (
+        config.get("tool", {}).get("pdm", {}).get("build", {})
+    )
+    for key, value in build_config.items():
+        context.config.build_config[key] = value
+    dependencies: List[str] = metadata.get("dependencies", [])
+    new_dependencies = []
+    for dep in dependencies:
+        if dep in sync_dependencies:
+            new_dep = f"{dep}=={version}"
+            new_dependencies.append(new_dep)
+        else:
+            new_dependencies.append(dep)
+    if new_dependencies != dependencies:
+        metadata["dependencies"] = new_dependencies
