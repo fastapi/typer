46ac20fd3c5c63b42ab20b2cf553ab4127583a98
Patrick Düggelin <patrick.dueggelin@live.de>
2024-04-24 11:20:08 +0200

    :white_check_mark: Add basic testcase

---
+++ tests/test_tutorial/test_parameter_types/test_pydantic/test_tutorial001.py
@@ -0,0 +1,33 @@
+import subprocess
+import sys
+
+import typer
+from typer.testing import CliRunner
+
+from docs_src.parameter_types.pydantic import tutorial001 as mod
+
+runner = CliRunner()
+
+app = typer.Typer()
+app.command()(mod.main)
+
+
+def test_help():
+    result = runner.invoke(app, ["--help"])
+    assert result.exit_code == 0
+
+
+
+def test_parse_pydantic_model():
+    result = runner.invoke(app, ["1", "--user-id", "2", "--user-name", "John Doe"])
+    assert "1 <class 'int'>" in result.output
+    assert "id=2 name='John Doe' <class 'docs_src.parameter_types.pydantic.tutorial001.User'>" in result.output
+
+
+def test_script():
+    result = subprocess.run(
+        [sys.executable, "-m", "coverage", "run", mod.__file__, "--help"],
+        capture_output=True,
+        encoding="utf-8",
+    )
+    assert "Usage" in result.stdout
