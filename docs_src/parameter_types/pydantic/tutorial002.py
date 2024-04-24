05fce89dadde60de4a0eed161cb65d7dda575254
Patrick Düggelin <patrick.dueggelin@live.de>
2024-04-24 22:41:11 +0200

    :white_check_mark: Tutorial and test nested model

---
+++ docs_src/parameter_types/pydantic/tutorial002.py
@@ -0,0 +1,24 @@
+from typing import List, Optional
+
+import typer
+
+import pydantic
+
+
+class Pet(pydantic.BaseModel):
+    name: str
+    species: str
+
+
+class Person(pydantic.BaseModel):
+    name: str
+    age: Optional[float] = None
+    pet: Pet
+
+
+def main(person: Person):
+    print(person, type(person))
+
+
+if __name__ == "__main__":
+    typer.run(main)
---
+++ tests/test_tutorial/test_parameter_types/test_pydantic/test_tutorial002.py
@@ -0,0 +1,34 @@
+import subprocess
+import sys
+
+import typer
+from typer.testing import CliRunner
+
+from docs_src.parameter_types.pydantic import tutorial002 as mod
+
+runner = CliRunner()
+
+app = typer.Typer()
+app.command()(mod.main)
+
+
+def test_help():
+    result = runner.invoke(app, ["--help"])
+    assert result.exit_code == 0
+
+
+def test_parse_pydantic_model():
+    result = runner.invoke(app, ["--person.name", "Jeff", "--person.pet.name", "Lassie", "--person.pet.species", "dog"])
+    assert (
+        "name='Jeff' age=None pet=Pet(name='Lassie', species='dog') <class 'docs_src.parameter_types.pydantic.tutorial002.Person'>"
+        in result.output
+    )
+
+
+def test_script():
+    result = subprocess.run(
+        [sys.executable, "-m", "coverage", "run", mod.__file__, "--help"],
+        capture_output=True,
+        encoding="utf-8",
+    )
+    assert "Usage" in result.stdout
