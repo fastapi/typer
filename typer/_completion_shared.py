a115c8dc414c999ba706ba3e65b23acdcaae0671
svlandeg <sofie.vanlandeghem@gmail.com>
2024-04-25 16:31:16 +0200

    revert the path.exists check (for now)

--- typer/_completion_shared.py
+++ typer/_completion_shared.py
@@ -183,16 +183,13 @@ def install_powershell(*, prog_name: str, complete_var: str, shell: str) -> Path
     if isinstance(result.stdout, str):  # pragma: no cover
         path_str = result.stdout
     if isinstance(result.stdout, bytes):
-        path_obj = None
         for encoding in ["windows-1252", "utf8", "cp850"]:
             try:
-                path_str = result.stdout.decode(encoding).strip()
-                if os.path.exists(path_str):
-                    path_obj = Path(path_str)
-                    break
+                path_str = result.stdout.decode(encoding)
+                break
             except UnicodeDecodeError:  # pragma: no cover
                 pass
-        if not path_obj:  # pragma: no cover
+        if not path_str:  # pragma: no cover
             click.echo("Couldn't decode the path automatically", err=True)
             raise click.exceptions.Exit(1)
     path_obj = Path(path_str.strip())
