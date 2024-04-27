3758343f00ccd74a4b59533d111a4a2bed92e7e0
Iván Todorovich <ivan.todorovich@gmail.com>
2024-04-27 19:44:04 -0300

    🌱 Run lint in a separate ci job
    Before this commit, the linting was done in the test matrix (once per python
    version). This is not necessary, as the linting is the same for all versions.
    
    Now, the linting is fully delegated to pre-commit, and executed as a separate
    job.

--- scripts/format.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-set -x
-set -e
-
-ruff check typer tests docs_src scripts --fix
-ruff format typer tests docs_src scripts
--- scripts/lint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-mypy typer
-ruff check typer tests docs_src
-ruff format typer tests docs_src --check
