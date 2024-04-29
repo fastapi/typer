ab857cbad1bc33e3465d796cb82928ea3bfff9ca
Iván Todorovich <ivan.todorovich@gmail.com>
2024-04-29 20:43:08 -0300

    🐛 Fix usage of `Annotated` with future annotations in Python 3.8
    Fixes https://github.com/tiangolo/typer/discussions/802
    
    `RuntimeError: Type not yet supported: typing_extensions.Annotated[...]`
    
    The solution is to use the `get_type_hints` method from `typing_extensions`
    instead of the built-in in the `typing` module, as it includes backports that
    support properly evaluating the forward reference.

--- typer/utils.py
+++ typer/utils.py
@@ -1,9 +1,9 @@
 import inspect
 import sys
 from copy import copy
-from typing import Any, Callable, Dict, List, Tuple, Type, cast, get_type_hints
+from typing import Any, Callable, Dict, List, Tuple, Type, cast
 
-from typing_extensions import Annotated
+from typing_extensions import Annotated, get_type_hints
 
 from ._typing import get_args, get_origin
 from .models import ArgumentInfo, OptionInfo, ParameterInfo, ParamMeta
