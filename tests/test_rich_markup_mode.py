8dd3b5bec96b6a864e05c3b59ffcf0954ad73a1c
svlandeg <svlandeg@github.com>
2024-08-29 15:34:02 +0200

    unit tests from #449 capturing current behaviour of this PR (WIP)

--- tests/test_rich_markup_mode.py
+++ tests/test_rich_markup_mode.py
@@ -111,3 +111,75 @@ def test_markup_mode_newline_issue447(mode: str, lines: List[str]):
     arg_start = [i for i, row in enumerate(result_lines) if "Arguments" in row][0]
     assert help_start != -1
     assert result_lines[help_start:arg_start] == lines
+
+
+@pytest.mark.parametrize(
+    "mode,lines",
+    [
+        ("markdown", ["First line", "", "", "• 1", "• 2", "• 3", ""]),
+        ("rich", ["First line", "", "- 1", "- 2", "- 3", ""]),
+        ("none", ["First line", "", "- 1 - 2 - 3", ""]),
+    ],
+)
+def test_markup_mode_bullets(mode: str, lines: List[str]):
+    app = typer.Typer(rich_markup_mode=mode)
+
+    @app.command()
+    def main(arg: str):
+        """First line
+
+        - 1
+        - 2
+        - 3
+        """
+        print(f"Hello {arg}")
+
+    assert app.rich_markup_mode == mode
+
+    result = runner.invoke(app, ["World"])
+    assert "Hello World" in result.stdout
+
+    result = runner.invoke(app, ["--help"])
+    result_lines = [line.strip() for line in result.stdout.split("\n")]
+    assert any(c in result.stdout for c in rounded)
+    help_start = result_lines.index("First line")
+    arg_start = [i for i, row in enumerate(result_lines) if "Arguments" in row][0]
+    assert help_start != -1
+    assert result_lines[help_start:arg_start] == lines
+
+
+@pytest.mark.parametrize(
+    "mode,lines",
+    [
+        ("markdown", ["First line", "", "", "• 1", "• 2", "• a", "• b", "• 3", ""]),
+        ("rich", ["First line", "", "- 1", "- 2", "- a", "- b", "- 3", ""]),
+        ("none", ["First line", "", "- 1 - 2   - a   - b - 3", ""]),
+    ],
+)
+def test_markup_mode_nested_bullets(mode: str, lines: List[str]):
+    app = typer.Typer(rich_markup_mode=mode)
+
+    @app.command()
+    def main(arg: str):
+        """First line
+
+        - 1
+        - 2
+          - a
+          - b
+        - 3
+        """
+        print(f"Hello {arg}")
+
+    assert app.rich_markup_mode == mode
+
+    result = runner.invoke(app, ["World"])
+    assert "Hello World" in result.stdout
+
+    result = runner.invoke(app, ["--help"])
+    result_lines = [line.strip() for line in result.stdout.split("\n")]
+    assert any(c in result.stdout for c in rounded)
+    help_start = result_lines.index("First line")
+    arg_start = [i for i, row in enumerate(result_lines) if "Arguments" in row][0]
+    assert help_start != -1
+    assert result_lines[help_start:arg_start] == lines
