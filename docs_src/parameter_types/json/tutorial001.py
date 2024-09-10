11292ff03ff46a9c7de9af941475db0d1b590e5b
Muhammed Hussein Karimi <info@karimi.dev>
2024-09-10 13:11:19 +0330

    📝 docs: added json arg type in docs_src
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

---
+++ docs_src/parameter_types/json/tutorial001.py
@@ -0,0 +1,11 @@
+import json
+import typer
+from typing import Annotated
+
+
+def main(user_info: Annotated[dict, typer.Option()]):
+    print(f"User Info: {json.dumps(user_info)}")
+
+
+if __name__ == "__main__":
+    typer.run(main)
