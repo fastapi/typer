f5f1ea48730b4e77447ca0fe44b43e74a118d59d
Sebastián Ramírez <tiangolo@gmail.com>
2024-11-07 17:18:14 +0000

    ✅ Move test to its own file

--- tests/test_annotated.py
+++ tests/test_annotated.py
@@ -76,35 +76,3 @@ def test_annotated_option_with_argname_doesnt_mutate_multiple_calls():
     result = runner.invoke(app, ["--force"])
     assert result.exit_code == 0, result.output
     assert "Forcing operation" in result.output
-
-
-def test_default_with_class_with_custom_eq():
-    app = typer.Typer()
-
-    from typer.models import ParamMeta
-
-    class StupidClass:
-        def __init__(self, a):
-            self.a = a
-
-        def __eq__(self, other) -> bool:
-            if other is ParamMeta.empty:
-                return True
-            try:
-                return self.a == other.a
-            except Exception:
-                return False
-
-        def __ne__(self, other: object) -> bool:
-            return not self.__eq__(other)
-
-    @app.command()
-    def cmd(val=StupidClass(42)):
-        print(val)
-
-    assert StupidClass(666) == ParamMeta.empty
-    assert StupidClass(666) != StupidClass(1)
-
-    result = runner.invoke(app)
-    assert result.exit_code == 0, result.output
-    assert "StupidClass" in result.output
