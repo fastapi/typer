da1d6918a95535ecff83cb24f9a577003de9ea2d
Danilo Horta <danilo.horta@pm.me>
2024-11-13 23:21:48 +0000

    Update typer/main.py
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>

--- typer/main.py
+++ typer/main.py
@@ -15,13 +15,7 @@ from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, Type, U
 from uuid import UUID
 
 import click
-
-if sys.version_info >= (3, 9):
-    from typing import get_args, get_origin
-else:
-    from typing_extensions import get_args, get_origin
-
-from ._typing import is_union
+from ._typing import is_union, get_args, get_origin
 from .completion import get_completion_inspect_parameters
 from .core import (
     DEFAULT_MARKUP_MODE,
