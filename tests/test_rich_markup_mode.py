2eae2b10052661b8dda6f07fb5b7aff82e18d24c
svlandeg <svlandeg@github.com>
2024-12-26 12:16:28 +0100

    change tests to represent current decisions on parsing newlines

--- tests/test_rich_markup_mode.py
+++ tests/test_rich_markup_mode.py
@@ -95,34 +95,41 @@ def test_markup_mode_newline_pr815(mode: str, lines: List[str]):
 @pytest.mark.parametrize(
     "mode,lines",
     [
-        (
+        pytest.param(
             "markdown",
             [
                 "This header is just pretty long really",
-                "Line 1 of a very extremely super long line And a short line 2",
+                "",
+                "Line 1 of a very extremely super long line",
+                "",
+                "And a short line 2",
                 "",
             ],
+            marks=pytest.mark.xfail,
         ),
-        (
+        pytest.param(
             "rich",
             [
                 "This header is just pretty long really",
-                "Line 1 of a very",
-                "extremely super long",
-                "line",
+                "",
+                "Line 1 of a very extremely super long line",
                 "",
                 "And a short line 2",
                 "",
             ],
+            marks=pytest.mark.xfail,
         ),
-        (
+        pytest.param(
             "none",
             [
                 "This header is just pretty long really",
+                "",
                 "Line 1 of a very extremely super long line",
+                "",
                 "And a short line 2",
                 "",
             ],
+            marks=pytest.mark.xfail,
         ),
     ],
 )
@@ -161,16 +168,17 @@ def test_markup_mode_newline_issue447(mode: str, lines: List[str]):
 @pytest.mark.parametrize(
     "mode,lines",
     [
+        ("markdown", ["First line", "", "• 1 - 2 - 3", ""]),
         pytest.param(
-            "markdown",
-            ["First line", "", "• 1", "• 2", "• 3", ""],
-            marks=pytest.mark.xfail(),
+            "rich", ["First line", "", "- 1 - 2 - 3", ""], marks=pytest.mark.xfail
         ),
-        ("rich", ["First line", "- 1", "- 2", "- 3", ""]),
-        ("none", ["First line", "- 1 - 2 - 3", ""]),
+        pytest.param(
+            "none", ["First line", "", "- 1 - 2 - 3", ""], marks=pytest.mark.xfail
+        ),
     ],
 )
-def test_markup_mode_bullets(mode: str, lines: List[str]):
+def test_markup_mode_bullets_single_newline(mode: str, lines: List[str]):
+    # Note: cf discussion at https://github.com/fastapi/typer/pull/964: we don't aim to support lists separated by a single line
     app = typer.Typer(rich_markup_mode=mode)
 
     @app.command()
@@ -200,16 +208,51 @@ def test_markup_mode_bullets(mode: str, lines: List[str]):
 @pytest.mark.parametrize(
     "mode,lines",
     [
+        ("markdown", ["First line", "", "• 1", "• 2", "• 3", ""]),
+        pytest.param("rich", ["First line", "", "- 1", "", "- 2", "", "- 3", ""], marks=pytest.mark.xfail),
         pytest.param(
-            "markdown",
-            ["First line", "", "• 1", "• 2", "• a", "• b", "• 3", ""],
-            marks=pytest.mark.xfail(),
+            "none", ["First line", "", "- 1", "", "- 2", "", "- 3", ""], marks=pytest.mark.xfail
         ),
-        ("rich", ["First line", "- 1", "- 2", "- a", "- b", "- 3", ""]),
-        ("none", ["First line", "- 1 - 2   - a   - b - 3", ""]),
     ],
 )
-def test_markup_mode_nested_bullets(mode: str, lines: List[str]):
+def test_markup_mode_bullets_double_newline(mode: str, lines: List[str]):
+    app = typer.Typer(rich_markup_mode=mode)
+
+    @app.command()
+    def main(arg: str):
+        """First line
+
+        - 1
+
+        - 2
+
+        - 3
+        """
+        print(f"Hello {arg}")
+
+    assert app.rich_markup_mode == mode
+
+    result = runner.invoke(app, ["World"])
+    assert "Hello World" in result.stdout
+
+    result = runner.invoke(app, ["--help"])
+    result_lines = [line.strip() for line in result.stdout.split("\n")]
+    assert any(c in result.stdout for c in rounded)
+    help_start = result_lines.index("First line")
+    arg_start = [i for i, row in enumerate(result_lines) if "Arguments" in row][0]
+    assert help_start != -1
+    assert result_lines[help_start:arg_start] == lines
+
+
+@pytest.mark.parametrize(
+    "mode,lines",
+    [
+        ("markdown", ["First line", "", "• 1 - 2   - a   - b - 3", ""]),
+        pytest.param("rich", ["First line", "", "- 1 - 2  - a  - b - 3", ""], marks=pytest.mark.xfail),
+        pytest.param("none", ["First line", "", "- 1 - 2  - a  - b - 3", ""], marks=pytest.mark.xfail),
+    ],
+)
+def test_markup_mode_nested_bullets_single_newline(mode: str, lines: List[str]):
     app = typer.Typer(rich_markup_mode=mode)
 
     @app.command()
@@ -220,6 +263,47 @@ def test_markup_mode_nested_bullets(mode: str, lines: List[str]):
         - 2
           - a
           - b
+        - 3
+        """
+        print(f"Hello {arg}")
+
+    assert app.rich_markup_mode == mode
+
+    result = runner.invoke(app, ["World"])
+    assert "Hello World" in result.stdout
+
+    result = runner.invoke(app, ["--help"])
+    result_lines = [line.strip() for line in result.stdout.split("\n")]
+    assert any(c in result.stdout for c in rounded)
+    help_start = result_lines.index("First line")
+    arg_start = [i for i, row in enumerate(result_lines) if "Arguments" in row][0]
+    assert help_start != -1
+    assert result_lines[help_start:arg_start] == lines
+
+
+@pytest.mark.parametrize(
+    "mode,lines",
+    [
+        ("markdown", ["First line", "", "• 1", "• 2", "• a", "• b", "• 3", ""]),
+        pytest.param("rich", ["First line", "", "- 1", "", "- 2", "", "- a", "", "- b", "", "- 3", ""], marks=pytest.mark.xfail),
+        pytest.param("none", ["First line", "", "- 1", "", "- 2", "", "- a", "", "- b", "", "- 3", ""], marks=pytest.mark.xfail),
+    ],
+)
+def test_markup_mode_nested_bullets_double_newline(mode: str, lines: List[str]):
+    app = typer.Typer(rich_markup_mode=mode)
+
+    @app.command()
+    def main(arg: str):
+        """First line
+
+        - 1
+
+        - 2
+
+          - a
+
+          - b
+
         - 3
         """
         print(f"Hello {arg}")
