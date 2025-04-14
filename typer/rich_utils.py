0e63bb4cddd386ca16a782c2cb6c5608602dc062
Kinuax <kinuax@protonmail.com>
2025-04-14 22:34:40 +0200

    Restore rich_utils

--- typer/rich_utils.py
+++ typer/rich_utils.py
@@ -2,6 +2,7 @@
 
 import inspect
 import io
+import sys
 from collections import defaultdict
 from gettext import gettext as _
 from os import getenv
@@ -21,7 +22,10 @@ from rich.table import Table
 from rich.text import Text
 from rich.theme import Theme
 
-from ._typing import Literal
+if sys.version_info >= (3, 9):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
 
 # Default styles
 STYLE_OPTION = "bold cyan"
