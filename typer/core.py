097d0723cada5f7c837b09889397aa67bab8c58b
Maurice Makaay <maurice@h2b.nl>
2025-07-28 20:04:03 +0200

    Fix the output for no_args_is_help=True
    Since typer 0.16.0, running a CLI with the no_args_is_help feature
    enabled displays an error block, e.g. for my application:
    
     Usage: myapp [OPTIONS] COMMAND [ARGS]...
    
     This is the CLI for my app
    
    ╭─ Options ─────────────────────────────────────────────────────────╮
    │ --verbose  -v        Use multiple times for more verbosity.       │
    │ --help               Show this message and exit.                  │
    ╰───────────────────────────────────────────────────────────────────╯
    ╭─ Commands ────────────────────────────────────────────────────────╮
    │ version   Show application version.                               │
    │ run       Start the application.                                  │
    ╰───────────────────────────────────────────────────────────────────╯
    Usage: authsync [OPTIONS] COMMAND [ARGS]...
    Try 'authsync --help' for help.
    ╭─ Error ───────────────────────────────────────────────────────────╮
    │                                                                   │
    ╰───────────────────────────────────────────────────────────────────╯
    
    The patch from the PR suppresses the duplicate Usage: info and the Error
    output block, in case the processed exception is a NoArgsIsHelperror.

--- typer/core.py
+++ typer/core.py
@@ -20,6 +20,7 @@ from typing import (
 
 import click
 import click.core
+from click.exceptions import NoArgsIsHelpError
 import click.formatting
 import click.parser
 import click.shell_completion
@@ -208,6 +209,8 @@ def _main(
             raise click.Abort() from e
         except KeyboardInterrupt as e:
             raise click.exceptions.Exit(130) from e
+        except NoArgsIsHelpError:
+            click.echo()
         except click.ClickException as e:
             if not standalone_mode:
                 raise
