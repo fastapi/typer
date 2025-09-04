fastapi/typer

commit f398fb1276062f9e77575e1b3e7570d43e5d7eb6

⚡️ Lazy-load `rich_utils` to reduce startup time (#1128)

typer/core.py
@@ -33,8 +33,6 @@ MarkupMode = Literal["markdown", "rich", None]
 try:
     import rich
 
-    from . import rich_utils
-
     DEFAULT_MARKUP_MODE: MarkupMode = "rich"
 
 except ImportError:  # pragma: no cover
@@ -213,6 +211,8 @@ def _main(
                 raise
             # Typer override
             if rich and rich_markup_mode is not None:
+                from . import rich_utils
+
                 rich_utils.rich_format_error(e)
             else:
                 e.show()
@@ -243,6 +243,8 @@ def _main(
             raise
         # Typer override
         if rich and rich_markup_mode is not None:
+            from . import rich_utils
+
             rich_utils.rich_abort_error()
         else:
             click.echo(_("Aborted!"), file=sys.stderr)
@@ -705,6 +707,8 @@ class TyperCommand(click.core.Command):
     def format_help(self, ctx: click.Context, formatter: click.HelpFormatter) -> None:
         if not rich or self.rich_markup_mode is None:
             return super().format_help(ctx, formatter)
+        from . import rich_utils
+
         return rich_utils.rich_format_help(
             obj=self,
             ctx=ctx,
@@ -768,6 +772,8 @@ class TyperGroup(click.core.Group):
     def format_help(self, ctx: click.Context, formatter: click.HelpFormatter) -> None:
         if not rich or self.rich_markup_mode is None:
             return super().format_help(ctx, formatter)
+        from . import rich_utils
+
         return rich_utils.rich_format_help(
             obj=self,
             ctx=ctx,

tests/assets/print_modules.py
@@ -1,0 +1,15 @@
+import sys
+
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def main():
+    for m in sys.modules:
+        print(m)
+
+
+if __name__ == "__main__":
+    app()

tests/test_rich_import.py
@@ -1,0 +1,21 @@
+import subprocess
+import sys
+from pathlib import Path
+
+ACCEPTED_MODULES = {"rich._extension", "rich"}
+
+
+def test_rich_not_imported_unnecessary():
+    file_path = Path(__file__).parent / "assets/print_modules.py"
+    result = subprocess.run(
+        [sys.executable, "-m", "coverage", "run", str(file_path)],
+        capture_output=True,
+        encoding="utf-8",
+    )
+    modules = result.stdout.splitlines()
+    modules = [
+        module
+        for module in modules
+        if module not in ACCEPTED_MODULES and module.startswith("rich")
+    ]
+    assert not modules

typer/cli.py
@@ -15,7 +15,6 @@ try:
     import rich
 
     has_rich = True
-    from . import rich_utils
 
 except ImportError:  # pragma: no cover
     has_rich = False
@@ -275,6 +274,8 @@ def get_docs_for_click(
 def _parse_html(input_text: str) -> str:
     if not has_rich:  # pragma: no cover
         return input_text
+    from . import rich_utils
+
     return rich_utils.rich_to_html(input_text)
 
 

typer/main.py
@@ -51,11 +51,6 @@ from .utils import get_params_from_function
 
 try:
     import rich
-    from rich.traceback import Traceback
-
-    from . import rich_utils
-
-    console_stderr = rich_utils._get_rich_console(stderr=True)
 
 except ImportError:  # pragma: no cover
     rich = None  # type: ignore
@@ -83,7 +78,9 @@ def except_hook(
     supress_internal_dir_names = [typer_path, click_path]
     exc = exc_value
     if rich:
-        from .rich_utils import MAX_WIDTH
+        from rich.traceback import Traceback
+
+        from . import rich_utils
 
         rich_tb = Traceback.from_exception(
             type(exc),
@@ -91,8 +88,9 @@ def except_hook(
             exc.__traceback__,
             show_locals=exception_config.pretty_exceptions_show_locals,
             suppress=supress_internal_dir_names,
-            width=MAX_WIDTH,
+            width=rich_utils.MAX_WIDTH,
         )
+        console_stderr = rich_utils._get_rich_console(stderr=True)
         console_stderr.print(rich_tb)
         return
     tb_exc = traceback.TracebackException.from_exception(exc)

commit 119c476321ca8ee26bfe8fbd20138910116a48a7

Make sure 'rich.markup' is imported when rendering help text
rich.markup is used to escape strings when rendering help texts.

This fixes a regression introduced by https://github.com/fastapi/typer/pull/1128

typer/core.py
@@ -372,6 +372,7 @@ class TyperArgument(click.core.Argument):
             extra_str = f"[{extra_str}]"
             if rich is not None:
                 # This is needed for when we want to export to HTML
+                import rich.markup
                 extra_str = rich.markup.escape(extra_str).strip()
 
             help = f"{help}  {extra_str}" if help else f"{extra_str}"
@@ -583,6 +584,7 @@ class TyperOption(click.core.Option):
             extra_str = f"[{extra_str}]"
             if rich is not None:
                 # This is needed for when we want to export to HTML
+                import rich.markup
                 extra_str = rich.markup.escape(extra_str).strip()
 
             help = f"{help}  {extra_str}" if help else f"{extra_str}"