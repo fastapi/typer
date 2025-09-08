221af6b05aef3b989a2496f77b5e1a40275bbcbe
Colin Nolan <cnolan@genpax.co>
2025-09-08 16:46:29 +0100

    Adds `TYPER_STANDARD_TRACEBACK` variable
    Keeps support fo `_TYPER_STANDARD_TRACEBACK` but notes as deprecated
    because:
    - It is non-standard for an advertised env to start with an underscore.
    - Environment variables that start with an underscore have been observed as
      causing issues in some execution environments (e.g. with AWS Lambda).
    
    See: https://github.com/fastapi/typer/discussions/1284

--- tests/test_tutorial/test_exceptions/test_tutorial002.py
+++ tests/test_tutorial/test_exceptions/test_tutorial002.py
@@ -16,7 +16,11 @@ def test_traceback_rich():
         [sys.executable, "-m", "coverage", "run", str(file_path), "secret"],
         capture_output=True,
         encoding="utf-8",
-        env={**os.environ, "_TYPER_STANDARD_TRACEBACK": ""},
+        env={
+            **os.environ,
+            "TYPER_STANDARD_TRACEBACK": "",
+            "_TYPER_STANDARD_TRACEBACK": "",
+        },
     )
     assert "return get_command(self)(*args, **kwargs)" not in result.stderr
 
@@ -32,7 +36,7 @@ def test_standard_traceback_env_var():
         [sys.executable, "-m", "coverage", "run", str(file_path), "secret"],
         capture_output=True,
         encoding="utf-8",
-        env={**os.environ, "_TYPER_STANDARD_TRACEBACK": "1"},
+        env={**os.environ, "TYPER_STANDARD_TRACEBACK": "1"},
     )
     assert "return get_command(self)(*args, **kwargs)" in result.stderr
 
