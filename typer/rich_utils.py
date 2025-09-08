b6f9778494e0aa5ad0552ef107661d1a616c9463
Nathan McDougall <nmcdougall@tonkintaylor.co.nz>
2025-09-08 18:12:44 +1200

    Move TID251 exclusion from inline `#noqa` comment to `pyproject.toml`

--- typer/rich_utils.py
+++ typer/rich_utils.py
@@ -1,7 +1,5 @@
 # Extracted and modified from https://github.com/ewels/rich-click
 
-# ruff: noqa: TID251 we are allowed to use rich in this module and assume it is available
-
 import inspect
 import io
 import sys
