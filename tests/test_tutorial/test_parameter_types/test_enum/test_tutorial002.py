2ebaa5e18397630dd0fec49d107087485a555c60
svlandeg <svlandeg@github.com>
2025-09-12 17:30:57 +0200

    use pytest.param to simplify test suite

--- tests/test_tutorial/test_parameter_types/test_enum/test_tutorial002.py
+++ tests/test_tutorial/test_parameter_types/test_enum/test_tutorial002.py
@@ -1,30 +1,47 @@
+import importlib
 import subprocess
 import sys
 
+import pytest
 import typer
+from typer import Typer
 from typer.testing import CliRunner
 
 from docs_src.parameter_types.enum import tutorial002 as mod
+from tests.utils import needs_py311
 
 runner = CliRunner()
 
-app = typer.Typer()
-app.command()(mod.main)
+
+@pytest.fixture(
+    name="app",
+    params=[
+        "tutorial002",
+        "tutorial002_an",
+        pytest.param("tutorial002_py311", marks=needs_py311),
+    ],
+)
+def get_app(request: pytest.FixtureRequest):
+    mod = importlib.import_module(f"docs_src.parameter_types.enum.{request.param}")
+
+    app = typer.Typer()
+    app.command()(mod.main)
+    return app
 
 
-def test_upper():
+def test_upper(app: Typer):
     result = runner.invoke(app, ["--network", "CONV"])
     assert result.exit_code == 0
     assert "Training neural network of type: conv" in result.output
 
 
-def test_mix():
+def test_mix(app: Typer):
     result = runner.invoke(app, ["--network", "LsTm"])
     assert result.exit_code == 0
     assert "Training neural network of type: lstm" in result.output
 
 
-def test_script():
+def test_script(app: Typer):
     result = subprocess.run(
         [sys.executable, "-m", "coverage", "run", mod.__file__, "--help"],
         capture_output=True,
