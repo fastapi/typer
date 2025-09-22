d93e8df3c8852441b972eebd07aaeee6e13323be
svlandeg <svlandeg@github.com>
2025-09-22 18:24:39 +0200

    import Annotated from typing_extensions

--- tests/test_others.py
+++ tests/test_others.py
@@ -14,6 +14,7 @@ from typer.core import _split_opt
 from typer.main import solve_typer_info_defaults, solve_typer_info_help
 from typer.models import ParameterInfo, TyperInfo
 from typer.testing import CliRunner
+from typing_extensions import Annotated
 
 from .utils import requires_completion_permission
 
@@ -178,9 +179,7 @@ def test_empty_list_default_generator():
 
     @app.command()
     def main(
-        names: typing.Annotated[
-            typing.List[str], typer.Option(default_factory=empty_list)
-        ],
+        names: Annotated[typing.List[str], typer.Option(default_factory=empty_list)],
     ):
         print(names)
 
