6cb94dd7a6cdd67d4aed34a20da08491cfc78264
Sofie Van Landeghem <svlandeg@users.noreply.github.com>
2025-11-19 12:01:32 +0100

    Fix typo

--- typer/rich_utils.py
+++ typer/rich_utils.py
@@ -235,7 +235,7 @@ def _get_parameter_help(
     ctx: click.Context,
     markup_mode: MarkupMode,
     rich_expand: bool = True,
-) -> Union[Table, Column]:
+) -> Union[Table, Columns]:
     """Build primary help text for a click option or argument.
 
     Returns the prose help text for an option or argument, rendered either
