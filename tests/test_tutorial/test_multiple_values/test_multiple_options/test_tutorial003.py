ac34bbd2ff73f9e99c3ad236e73971607194c023
svlandeg <svlandeg@github.com>
2025-11-25 14:23:00 +0100

    update tests

--- tests/test_tutorial/test_multiple_values/test_multiple_options/test_tutorial003.py
+++ tests/test_tutorial/test_multiple_values/test_multiple_options/test_tutorial003.py
@@ -1,14 +1,12 @@
 import subprocess
 import sys
 
-import typer
 from typer.testing import CliRunner
 
 from docs_src.multiple_values.multiple_options import tutorial003 as mod
+app = mod.app
 
 runner = CliRunner()
-app = typer.Typer()
-app.command()(mod.main)
 
 
 def test_main():
