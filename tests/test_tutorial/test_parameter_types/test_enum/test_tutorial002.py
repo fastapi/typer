6a3d9ba20182f980a0bac0ff0cd7c9aa085a4589
svlandeg <svlandeg@github.com>
2025-11-26 12:59:34 +0100

    use mod as fixture

--- tests/test_tutorial/test_parameter_types/test_enum/test_tutorial002.py
+++ tests/test_tutorial/test_parameter_types/test_enum/test_tutorial002.py
@@ -7,14 +7,13 @@ import typer
 from typer import Typer
 from typer.testing import CliRunner
 
-from docs_src.parameter_types.enum import tutorial002 as mod
 from tests.utils import needs_py311
 
 runner = CliRunner()
 
 
 @pytest.fixture(
-    name="app",
+    name="mod",
     params=[
         "tutorial002",
         "tutorial002_an",
@@ -23,25 +22,22 @@ runner = CliRunner()
 )
 def get_app(request: pytest.FixtureRequest):
     mod = importlib.import_module(f"docs_src.parameter_types.enum.{request.param}")
-
-    app = typer.Typer()
-    app.command()(mod.main)
-    return app
+    return mod
 
 
-def test_upper(app: Typer):
-    result = runner.invoke(app, ["--network", "CONV"])
+def test_upper(mod):
+    result = runner.invoke(mod.app, ["--network", "CONV"])
     assert result.exit_code == 0
     assert "Training neural network of type: conv" in result.output
 
 
-def test_mix(app: Typer):
-    result = runner.invoke(app, ["--network", "LsTm"])
+def test_mix(mod):
+    result = runner.invoke(mod.app, ["--network", "LsTm"])
     assert result.exit_code == 0
     assert "Training neural network of type: lstm" in result.output
 
 
-def test_script(app: Typer):
+def test_script(mod):
     result = subprocess.run(
         [sys.executable, "-m", "coverage", "run", mod.__file__, "--help"],
         capture_output=True,
