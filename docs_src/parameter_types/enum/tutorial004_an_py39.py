f08ba23fe777622a6fc77c398960906aca7aeb3e
Yurii Motov <yurii.motov.monte@gmail.com>
2025-12-22 13:50:10 +0100

    Update `docs_src/parameter_types`

---
+++ docs_src/parameter_types/enum/tutorial004_an_py39.py
@@ -0,0 +1,17 @@
+from typing import Annotated
+
+import typer
+from typing_extensions import Literal
+
+app = typer.Typer()
+
+
+@app.command()
+def main(
+    network: Annotated[Literal["simple", "conv", "lstm"], typer.Option()] = "simple",
+):
+    print(f"Training neural network of type: {network}")
+
+
+if __name__ == "__main__":
+    app()
