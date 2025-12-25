5a1e36049f49116dc24071189e59c37b6f752e94
Yurii Motov <yurii.motov.monte@gmail.com>
2025-12-25 22:07:17 +0100

    Add tests for `docs_src/options_autocompletion/tutorial001_*.py`

---
+++ tests/test_tutorial/test_options_autocompletion/test_tutorial001.py
@@ -0,0 +1,37 @@
+import importlib
+import subprocess
+import sys
+from types import ModuleType
+
+import pytest
+from typer.testing import CliRunner
+
+runner = CliRunner()
+
+
+@pytest.fixture(
+    name="mod",
+    params=[
+        pytest.param("tutorial001_py39"),
+        pytest.param("tutorial001_an_py39"),
+    ],
+)
+def get_mod(request: pytest.FixtureRequest) -> ModuleType:
+    module_name = f"docs_src.options_autocompletion.{request.param}"
+    mod = importlib.import_module(module_name)
+    return mod
+
+
+def test_1(mod: ModuleType):
+    result = runner.invoke(mod.app, ["--name", "Camila"])
+    assert result.exit_code == 0
+    assert "Hello Camila" in result.output
+
+
+def test_script(mod: ModuleType):
+    result = subprocess.run(
+        [sys.executable, "-m", "coverage", "run", mod.__file__, "--help"],
+        capture_output=True,
+        encoding="utf-8",
+    )
+    assert "Usage" in result.stdout
