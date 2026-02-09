cfda15483d79b96bbe8c95191fce255a54184550
svlandeg <svlandeg@github.com>
2026-02-09 17:51:41 +0100

    fix stderr test

--- tests/test_tutorial/test_printing/test_tutorial004.py
+++ tests/test_tutorial/test_printing/test_tutorial004.py
@@ -1,8 +1,6 @@
-import platform
 import subprocess
 import sys
 
-import pytest
 from typer.testing import CliRunner
 
 import docs_src.printing.tutorial004_py39 as mod
@@ -12,12 +10,10 @@ app = mod.app
 runner = CliRunner()
 
 
-@pytest.mark.xfail(
-    condition=((platform.system() == "Windows") and (sys.version_info < (3, 10))),
-    reason="On Windows with Python 3.9, output is in stdout instead of stderr",
-)
 def test_cli():
-    result = runner.invoke(app)
+    # Use mix_stderr=False so stderr is captured separately from stdout
+    run = CliRunner(mix_stderr=False)
+    result = run.invoke(app)
     assert result.exit_code == 0
     assert result.stdout == ""
     assert "Here is something written to standard error" in result.stderr
