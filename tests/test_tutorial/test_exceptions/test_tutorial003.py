458325db1de5b51c661e0fa65c591dd598bb6469
Sebastián Ramírez <tiangolo@gmail.com>
2026-02-11 07:17:36 -0800

    ♻️ When printing error tracebacks with Rich, default to not showing locals, which are sometimes verbose (#1072)
    Co-authored-by: svlandeg <sofie.vanlandeghem@gmail.com>
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>
    Co-authored-by: svlandeg <svlandeg@github.com>

--- tests/test_tutorial/test_exceptions/test_tutorial003.py
+++ tests/test_tutorial/test_exceptions/test_tutorial003.py
@@ -27,7 +27,7 @@ def test_traceback_rich_pretty_short_disable():
     assert "app()" in result.stderr
     assert "print(name + 3)" in result.stderr
     assert 'TypeError: can only concatenate str (not "int") to str' in result.stderr
-    assert "name = 'morty'" in result.stderr
+    assert "name = 'morty'" not in result.stderr
 
 
 def test_script():
