f78a84051f959b80f179b033dca05e3e7c264c20
Sebastián Ramírez <tiangolo@gmail.com>
2026-02-11 11:31:33 +0100

    ♻️ Update config and PDM hook to not require a config for each package, make typer-slim a wrapper, without code

--- pdm_build.py
+++ pdm_build.py
@@ -15,18 +15,15 @@ def pdm_build_initialize(context: Context) -> None:
         "_internal-slim-build"
     ]["sync-dependencies"]
     # Get custom config for the current package, from the env var
-    config: dict[str, Any] = context.config.data["tool"]["tiangolo"][
+    all_configs_config: dict[str, Any] = context.config.data["tool"]["tiangolo"][
         "_internal-slim-build"
-    ]["packages"][TIANGOLO_BUILD_PACKAGE]
+    ]["packages"]
+
+    if TIANGOLO_BUILD_PACKAGE not in all_configs_config:
+        return
+
+    config = all_configs_config[TIANGOLO_BUILD_PACKAGE]
     project_config: dict[str, Any] = config["project"]
-    # Get main optional dependencies, extras
-    optional_dependencies: dict[str, list[str]] = metadata.get(
-        "optional-dependencies", {}
-    )
-    # Get custom optional dependencies name to always include in this (non-slim) package
-    include_optional_dependencies: list[str] = config.get(
-        "include-optional-dependencies", []
-    )
     # Override main [project] configs with custom configs for this package
     for key, value in project_config.items():
         metadata[key] = value
@@ -39,10 +36,6 @@ def pdm_build_initialize(context: Context) -> None:
         context.config.build_config[key] = value
     # Get main dependencies
     dependencies: list[str] = metadata.get("dependencies", [])
-    # Add optional dependencies to the default dependencies for this (non-slim) package
-    for include_optional in include_optional_dependencies:
-        optional_dependencies_group = optional_dependencies.get(include_optional, [])
-        dependencies.extend(optional_dependencies_group)
     # Sync versions in dependencies
     new_dependencies = []
     for dep in dependencies:
