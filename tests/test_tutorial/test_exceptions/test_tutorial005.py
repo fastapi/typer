4f0f19589eb0400df082340806e869c04af8a54e
Fredrik Mellstrom <11281108+harkabeeparolus@users.noreply.github.com>
2026-02-18 07:33:23 +0100

    Rename tutorial to _py310

--- tests/test_tutorial/test_exceptions/test_tutorial005.py
+++ tests/test_tutorial/test_exceptions/test_tutorial005.py
@@ -10,9 +10,16 @@ _gitlab.Gitlab = MagicMock  # type: ignore[attr-defined]
 _gitlab.__file__ = "gitlab/__init__.py"  # type: ignore[attr-defined]
 sys.modules.setdefault("gitlab", _gitlab)
 
-from docs_src.exceptions import tutorial005_py39 as mod  # noqa: E402
+from docs_src.exceptions import tutorial005_py310 as mod  # noqa: E402
 
 runner = CliRunner()
+
+# There's no way to test a third-party package from PyPI. Also, the actual
+# feature we use is part of Rich. We just pass the flag along. So here we test
+# that the tutorial code runs without errors.
+
+# Perhaps we could find some standard library module that throws a long
+# traceback and test that instead, but for now this is probably good enough.
 
 
 def test_pretty_exceptions_suppress():
