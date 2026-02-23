71d0e3d4d121f87681b914c65e8777734dc5797c
svlandeg <sofie.vanlandeghem@gmail.com>
2026-02-23 12:22:01 +0100

    remove KeepOpenFile

--- typer/_click/utils.py
+++ typer/_click/utils.py
@@ -195,31 +195,6 @@ class LazyFile:
         return iter(self._f)  # type: ignore
 
 
-class KeepOpenFile:
-    def __init__(self, file: t.IO[t.Any]) -> None:
-        self._file: t.IO[t.Any] = file
-
-    def __getattr__(self, name: str) -> t.Any:
-        return getattr(self._file, name)
-
-    def __enter__(self) -> KeepOpenFile:
-        return self
-
-    def __exit__(
-        self,
-        exc_type: type[BaseException] | None,
-        exc_value: BaseException | None,
-        tb: TracebackType | None,
-    ) -> None:
-        pass
-
-    def __repr__(self) -> str:
-        return repr(self._file)
-
-    def __iter__(self) -> cabc.Iterator[t.AnyStr]:
-        return iter(self._file)
-
-
 def echo(
     message: t.Any | None = None,
     file: t.IO[t.Any] | None = None,
