4f15543428460ce21f42476c1e26037c7f477f32
Sofie Van Landeghem <svlandeg@users.noreply.github.com>
2026-03-16 13:29:42 +0100

    remove redundant cast

--- typer/utils.py
+++ typer/utils.py
@@ -144,7 +144,7 @@ def get_params_from_function(func: Callable[..., Any]) -> dict[str, ParamMeta]:
                 and isinstance(parameter_info.default, str)
             ):
                 parameter_info.param_decls = (
-                    cast(str, parameter_info.default),
+                    str, parameter_info.default,
                     *(parameter_info.param_decls or ()),
                 )
                 parameter_info.default = ...
