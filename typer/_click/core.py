13fa1c37372d13c71b09b4deb817cfac57250732
svlandeg <svlandeg@github.com>
2026-04-14 11:24:21 +0200

    make Command abstract and clean up

--- typer/_click/core.py
+++ typer/_click/core.py
@@ -2,11 +2,10 @@ from __future__ import annotations
 
 import collections.abc as cabc
 import enum
-import errno
 import inspect
 import os
-import sys
 import typing as t
+from abc import ABC, abstractmethod
 from contextlib import AbstractContextManager, ExitStack, contextmanager
 from gettext import gettext as _
 from gettext import ngettext
@@ -16,7 +15,6 @@ from . import types
 from .exceptions import (
     Abort,
     BadParameter,
-    ClickException,
     Exit,
     MissingParameter,
     NoArgsIsHelpError,
@@ -27,9 +25,6 @@ from .globals import pop_context, push_context
 from .parser import _OptionParser
 from .termui import style
 from .utils import (
-    PacifyFlushWrapper,
-    _detect_program_name,
-    _expand_args,
     echo,
     make_default_short_help,
 )
@@ -798,61 +793,15 @@ class Context:
         return self._parameter_source.get(name)
 
 
-class Command:
+class Command(ABC):
     """Commands are the basic building block of command line interfaces in
     Click.  A basic command handles command line parsing and might dispatch
     more parsing to commands nested below it.
-
-    :param name: the name of the command to use unless a group overrides it.
-    :param context_settings: an optional dictionary with defaults that are
-                             passed to the context object.
-    :param callback: the callback to invoke.  This is optional.
-    :param params: the parameters to register with this command.  This can
-                   be either :class:`Option` or :class:`Argument` objects.
-    :param help: the help string to use for this command.
-    :param epilog: like the help string but it's printed at the end of the
-                   help page after everything else.
-    :param short_help: the short help to use for this command.  This is
-                       shown on the command listing of the parent command.
-    :param add_help_option: by default each command registers a ``--help``
-                            option.  This can be disabled by this parameter.
-    :param no_args_is_help: this controls what happens if no arguments are
-                            provided.  This option is disabled by default.
-                            If enabled this will add ``--help`` as argument
-                            if no arguments are passed
-    :param hidden: hide this command from help outputs.
-    :param deprecated: If ``True`` or non-empty string, issues a message
-                        indicating that the command is deprecated and highlights
-                        its deprecation in --help. The message can be customized
-                        by using a string as the value.
-
-    .. versionchanged:: 8.1
-        ``help``, ``epilog``, and ``short_help`` are stored unprocessed,
-        all formatting is done when outputting help text, not at init,
-        and is done even if not using the ``@command`` decorator.
-
-    .. versionchanged:: 8.0
-        Added a ``repr`` showing the command name.
-
-    .. versionchanged:: 7.1
-        Added the ``no_args_is_help`` parameter.
-
-    .. versionchanged:: 2.0
-        Added the ``context_settings`` parameter.
     """
 
-    #: The context class to create with :meth:`make_context`.
-    #:
-    #: .. versionadded:: 8.0
     context_class: type[Context] = Context
-
-    #: the default for the :attr:`Context.allow_extra_args` flag.
     allow_extra_args = False
-
-    #: the default for the :attr:`Context.allow_interspersed_args` flag.
     allow_interspersed_args = True
-
-    #: the default for the :attr:`Context.ignore_unknown_options` flag.
     ignore_unknown_options = False
 
     def __init__(
@@ -870,24 +819,14 @@ class Command:
         hidden: bool = False,
         deprecated: bool | str = False,
     ) -> None:
-        #: the name the command thinks it has.  Upon registering a command
-        #: on a :class:`Group` the group will default the command name
-        #: with this information.  You should instead use the
-        #: :class:`Context`\'s :attr:`~Context.info_name` attribute.
         self.name = name
 
         if context_settings is None:
             context_settings = {}
 
-        #: an optional dictionary with defaults passed to the context.
         self.context_settings: cabc.MutableMapping[str, t.Any] = context_settings
 
-        #: the callback to execute when the command fires.  This might be
-        #: `None` in which case nothing happens.
         self.callback = callback
-        #: the list of parameters for this command in the order they
-        #: should show up in the help page and execute.  Eager parameters
-        #: will automatically be handled before non eager ones.
         self.params: list[Parameter] = params or []
         self.help = help
         self.epilog = epilog
@@ -903,10 +842,7 @@ class Command:
         return f"<{self.__class__.__name__} {self.name}>"
 
     def get_usage(self, ctx: Context) -> str:
-        """Formats the usage line into a string and returns it.
-
-        Calls :meth:`format_usage` internally.
-        """
+        """Formats the usage line into a string and returns it."""
         formatter = ctx.make_formatter()
         self.format_usage(ctx, formatter)
         return formatter.getvalue().rstrip("\n")
@@ -921,10 +857,7 @@ class Command:
         return params
 
     def format_usage(self, ctx: Context, formatter: HelpFormatter) -> None:
-        """Writes the usage line into the formatter.
-
-        This is a low-level method called by :meth:`get_usage`.
-        """
+        """Writes the usage line into the formatter."""
         pieces = self.collect_usage_pieces(ctx)
         formatter.write_usage(ctx.command_path, " ".join(pieces))
 
@@ -948,13 +881,7 @@ class Command:
         return list(all_names)
 
     def get_help_option(self, ctx: Context) -> TyperOption | None:
-        """Returns the help option object.
-
-        Skipped if :attr:`add_help_option` is ``False``.
-
-        .. versionchanged:: 8.1.8
-            The help option is now cached to avoid creating it multiple times.
-        """
+        """Returns the help option object."""
         help_option_names = self.get_help_option_names(ctx)
 
         if not help_option_names or not self.add_help_option:
@@ -982,10 +909,7 @@ class Command:
         return parser
 
     def get_help(self, ctx: Context) -> str:
-        """Formats the help into a string and returns it.
-
-        Calls :meth:`format_help` internally.
-        """
+        """Formats the help into a string and returns it."""
         formatter = ctx.make_formatter()
         self.format_help(ctx, formatter)
         return formatter.getvalue().rstrip("\n")
@@ -1014,17 +938,7 @@ class Command:
         return text.strip()
 
     def format_help(self, ctx: Context, formatter: HelpFormatter) -> None:
-        """Writes the help into the formatter if it exists.
-
-        This is a low-level method called by :meth:`get_help`.
-
-        This calls the following methods:
-
-        -   :meth:`format_usage`
-        -   :meth:`format_help_text`
-        -   :meth:`format_options`
-        -   :meth:`format_epilog`
-        """
+        """Writes the help into the formatter if it exists."""
         self.format_usage(ctx, formatter)
         self.format_help_text(ctx, formatter)
         self.format_options(ctx, formatter)
@@ -1054,17 +968,9 @@ class Command:
             with formatter.indentation():
                 formatter.write_text(text)
 
+    @abstractmethod
     def format_options(self, ctx: Context, formatter: HelpFormatter) -> None:
-        """Writes all the options into the formatter if they exist."""
-        opts = []
-        for param in self.get_params(ctx):
-            rv = param.get_help_record(ctx)
-            if rv is not None:
-                opts.append(rv)
-
-        if opts:
-            with formatter.section(_("Options")):
-                formatter.write_dl(opts)
+        pass
 
     def format_epilog(self, ctx: Context, formatter: HelpFormatter) -> None:
         """Writes the epilog into the formatter if it exists."""
@@ -1088,19 +994,6 @@ class Command:
 
         To quickly customize the context class used without overriding
         this method, set the :attr:`context_class` attribute.
-
-        :param info_name: the info name for this invocation.  Generally this
-                          is the most descriptive name for the script or
-                          command.  For the toplevel script it's usually
-                          the name of the script, for commands below it's
-                          the name of the command.
-        :param args: the arguments to parse as list of strings.
-        :param parent: the parent context if available.
-        :param extra: extra keyword arguments forwarded to the context
-                      constructor.
-
-        .. versionchanged:: 8.0
-            Added the :attr:`context_class` attribute.
         """
         for key, value in self.context_settings.items():
             if key not in extra:
@@ -1157,11 +1050,6 @@ class Command:
 
         Any command could be part of a chained multi-command, so sibling
         commands are valid at any point during command completion.
-
-        :param ctx: Invocation context for this command.
-        :param incomplete: Value being completed. May be empty.
-
-        .. versionadded:: 8.0
         """
         # avoid circular imports
         from .shell_completion import CompletionItem
@@ -1192,26 +1080,7 @@ class Command:
 
         return results
 
-    @t.overload
-    def main(
-        self,
-        args: cabc.Sequence[str] | None = None,
-        prog_name: str | None = None,
-        complete_var: str | None = None,
-        standalone_mode: t.Literal[True] = True,
-        **extra: t.Any,
-    ) -> t.NoReturn: ...
-
-    @t.overload
-    def main(
-        self,
-        args: cabc.Sequence[str] | None = None,
-        prog_name: str | None = None,
-        complete_var: str | None = None,
-        standalone_mode: bool = ...,
-        **extra: t.Any,
-    ) -> t.Any: ...
-
+    @abstractmethod
     def main(
         self,
         args: cabc.Sequence[str] | None = None,
@@ -1221,144 +1090,19 @@ class Command:
         windows_expand_args: bool = True,
         **extra: t.Any,
     ) -> t.Any:
-        """This is the way to invoke a script with all the bells and
-        whistles as a command line application.  This will always terminate
-        the application after a call.  If this is not wanted, ``SystemExit``
-        needs to be caught.
+        pass
 
-        This method is also available by directly calling the instance of
-        a :class:`Command`.
-
-        :param args: the arguments that should be used for parsing.  If not
-                     provided, ``sys.argv[1:]`` is used.
-        :param prog_name: the program name that should be used.  By default
-                          the program name is constructed by taking the file
-                          name from ``sys.argv[0]``.
-        :param complete_var: the environment variable that controls the
-                             bash completion support.  The default is
-                             ``"_<prog_name>_COMPLETE"`` with prog_name in
-                             uppercase.
-        :param standalone_mode: the default behavior is to invoke the script
-                                in standalone mode.  Click will then
-                                handle exceptions and convert them into
-                                error messages and the function will never
-                                return but shut down the interpreter.  If
-                                this is set to `False` they will be
-                                propagated to the caller and the return
-                                value of this function is the return value
-                                of :meth:`invoke`.
-        :param windows_expand_args: Expand glob patterns, user dir, and
-            env vars in command line args on Windows.
-        :param extra: extra keyword arguments are forwarded to the context
-                      constructor.  See :class:`Context` for more information.
-
-        .. versionchanged:: 8.0.1
-            Added the ``windows_expand_args`` parameter to allow
-            disabling command line arg expansion on Windows.
-
-        .. versionchanged:: 8.0
-            When taking arguments from ``sys.argv`` on Windows, glob
-            patterns, user dir, and env vars are expanded.
-
-        .. versionchanged:: 3.0
-           Added the ``standalone_mode`` parameter.
-        """
-        if args is None:
-            args = sys.argv[1:]
-
-            if os.name == "nt" and windows_expand_args:
-                args = _expand_args(args)
-        else:
-            args = list(args)
-
-        if prog_name is None:
-            prog_name = _detect_program_name()
-
-        # Process shell completion requests and exit early.
-        self._main_shell_completion(extra, prog_name, complete_var)
-
-        try:
-            try:
-                with self.make_context(prog_name, args, **extra) as ctx:
-                    rv = self.invoke(ctx)
-                    if not standalone_mode:
-                        return rv
-                    # it's not safe to `ctx.exit(rv)` here!
-                    # note that `rv` may actually contain data like "1" which
-                    # has obvious effects
-                    # more subtle case: `rv=[None, None]` can come out of
-                    # chained commands which all returned `None` -- so it's not
-                    # even always obvious that `rv` indicates success/failure
-                    # by its truthiness/falsiness
-                    ctx.exit()
-            except (EOFError, KeyboardInterrupt) as e:
-                echo(file=sys.stderr)
-                raise Abort() from e
-            except ClickException as e:
-                if not standalone_mode:
-                    raise
-                e.show()
-                sys.exit(e.exit_code)
-            except OSError as e:
-                if e.errno == errno.EPIPE:
-                    sys.stdout = t.cast(t.TextIO, PacifyFlushWrapper(sys.stdout))
-                    sys.stderr = t.cast(t.TextIO, PacifyFlushWrapper(sys.stderr))
-                    sys.exit(1)
-                else:
-                    raise
-        except Exit as e:
-            if standalone_mode:
-                sys.exit(e.exit_code)
-            else:
-                # in non-standalone mode, return the exit code
-                # note that this is only reached if `self.invoke` above raises
-                # an Exit explicitly -- thus bypassing the check there which
-                # would return its result
-                # the results of non-standalone execution may therefore be
-                # somewhat ambiguous: if there are codepaths which lead to
-                # `ctx.exit(1)` and to `return 1`, the caller won't be able to
-                # tell the difference between the two
-                return e.exit_code
-        except Abort:
-            if not standalone_mode:
-                raise
-            echo(_("Aborted!"), file=sys.stderr)
-            sys.exit(1)
-
+    @abstractmethod
     def _main_shell_completion(
         self,
         ctx_args: cabc.MutableMapping[str, t.Any],
         prog_name: str,
         complete_var: str | None = None,
     ) -> None:
-        """Check if the shell is asking for tab completion, process
-        that, then exit early. Called from :meth:`main` before the
-        program is invoked.
-
-        :param prog_name: Name of the executable in the shell.
-        :param complete_var: Name of the environment variable that holds
-            the completion instruction. Defaults to
-            ``_{PROG_NAME}_COMPLETE``.
-
-        .. versionchanged:: 8.2.0
-            Dots (``.``) in ``prog_name`` are replaced with underscores (``_``).
-        """
-        if complete_var is None:
-            complete_name = prog_name.replace("-", "_").replace(".", "_")
-            complete_var = f"_{complete_name}_COMPLETE".upper()
-
-        instruction = os.environ.get(complete_var)
-
-        if not instruction:
-            return
-
-        from .shell_completion import shell_complete
-
-        rv = shell_complete(self, ctx_args, prog_name, complete_var, instruction)
-        sys.exit(rv)
+        pass
 
     def __call__(self, *args: t.Any, **kwargs: t.Any) -> t.Any:
-        """Alias for :meth:`main`."""
+        """Alias for self.main"""
         return self.main(*args, **kwargs)
 
 
