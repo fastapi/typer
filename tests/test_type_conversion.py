c4748aeef3d3a03f4252d705f0a2db0a12aae56c
svlandeg <sofie.vanlandeghem@gmail.com>
2026-07-22 12:12:43 +0200

    fix duplicate test

--- tests/test_type_conversion.py
+++ tests/test_type_conversion.py
@@ -366,67 +366,6 @@ def test_default_infers_param_type(
         assert isinstance(seen["val"], tuple)
 
 
-@pytest.mark.parametrize(
-    ("parameter", "expected_metavar"),
-    [
-        pytest.param(Annotated[str, typer.Option(...)], "<str>"),
-        pytest.param(Annotated[str, typer.Argument(...)], "<str>"),
-        pytest.param(Annotated[int, typer.Option(...)], "<int>"),
-        pytest.param(Annotated[int, typer.Argument(...)], "<int>"),
-        pytest.param(Annotated[float, typer.Option(...)], "<float>"),
-        pytest.param(
-            Annotated[float, typer.Option(..., min=0.666, max=3.42)], "<float range>"
-        ),
-        pytest.param(Annotated[bytes, typer.Option(...)], "<bytes>"),
-        pytest.param(Annotated[list[str], typer.Option(...)], "<list[str]>"),
-        pytest.param(Annotated[tuple[str, int], typer.Option(...)], "<str,int>..."),
-        pytest.param(Annotated[tuple[Path, str], typer.Option(...)], "<Path,str>..."),
-        pytest.param(Annotated[str, typer.Option(..., resolve_path=True)], "<str>"),
-        pytest.param(Annotated[Path, typer.Option(...)], "<path>"),
-        pytest.param(Annotated[Path, typer.Option(..., dir_okay=False)], "<file>"),
-        pytest.param(Annotated[Path, typer.Option(..., file_okay=False)], "<dir>"),
-        pytest.param(Annotated[SomeEnum, typer.Option(...)], "<one|two|three>"),
-        pytest.param(Annotated[SomeEnum, typer.Argument()], "<one|two|three>"),
-        pytest.param(
-            Annotated[SomeEnum, typer.Option(..., show_choices=False)], "<SomeEnum>"
-        ),
-        pytest.param(
-            Annotated[list[SomeEnum], typer.Option(...)], "<list[one|two|three]>"
-        ),
-        pytest.param(
-            Annotated[list[SomeEnum], typer.Option(..., show_choices=False)],
-            "<list[SomeEnum]>",
-        ),
-        pytest.param(Annotated[Literal["x", "y"], typer.Option(...)], "<x|y>"),
-        pytest.param(Annotated[typer.FileText, typer.Option(...)], "<FileText>"),
-        pytest.param(Annotated[datetime, typer.Option(...)], "<%Y-%m-%d>"),
-        pytest.param(
-            Annotated[datetime, typer.Option(..., formats=["%Y-%m-%d", "%d/%m/%Y"])],
-            "<%Y-%m-%d|%d/%m/%Y>",
-        ),
-    ],
-)
-def test_param_type_help_metavar(parameter: Any, expected_metavar: str) -> None:
-    app = typer.Typer()
-
-    @app.command()
-    # TODO: type-specific default
-    def with_default(value: parameter = "my_default"):
-        pass  # pragma: no cover
-
-    @app.command()
-    def without_default(value: parameter):
-        pass  # pragma: no cover
-
-    result = runner.invoke(app, ["with-default", "--help"])
-    assert result.exit_code == 0
-    assert expected_metavar in result.output
-
-    result = runner.invoke(app, ["without-default", "--help"])
-    assert result.exit_code == 0
-    assert expected_metavar in result.output
-
-
 def test_int_rejects_float_default() -> None:
     app = typer.Typer()
 
@@ -488,6 +427,8 @@ def test_argv_encoding(
         pytest.param(
             Annotated[float, typer.Option(..., min=0.666, max=3.42)], "<float range>"
         ),
+        pytest.param(Annotated[bytes, typer.Option(...)], "<bytes>"),
+        pytest.param(Annotated[list[str], typer.Option(...)], "<list[str]>"),
         pytest.param(Annotated[tuple[str, int], typer.Option(...)], "<str,int>..."),
         pytest.param(Annotated[tuple[Path, str], typer.Option(...)], "<Path,str>..."),
         pytest.param(Annotated[str, typer.Option(..., resolve_path=True)], "<str>"),
@@ -496,7 +437,19 @@ def test_argv_encoding(
         pytest.param(Annotated[Path, typer.Option(..., file_okay=False)], "<dir>"),
         pytest.param(Annotated[SomeEnum, typer.Option(...)], "<one|two|three>"),
         pytest.param(Annotated[SomeEnum, typer.Argument()], "<one|two|three>"),
+        pytest.param(
+            Annotated[SomeEnum, typer.Option(..., show_choices=False)], "<SomeEnum>"
+        ),
+        pytest.param(
+            Annotated[list[SomeEnum], typer.Option(...)], "<list[one|two|three]>"
+        ),
+        pytest.param(
+            Annotated[list[SomeEnum], typer.Option(..., show_choices=False)],
+            "<list[SomeEnum]>",
+        ),
         pytest.param(Annotated[Literal["x", "y"], typer.Option(...)], "<x|y>"),
+        pytest.param(Annotated[typer.FileText, typer.Option(...)], "<FileText>"),
+        pytest.param(Annotated[datetime, typer.Option(...)], "<%Y-%m-%d>"),
         pytest.param(
             Annotated[datetime, typer.Option(..., formats=["%Y-%m-%d", "%d/%m/%Y"])],
             "<%Y-%m-%d|%d/%m/%Y>",
@@ -507,7 +460,6 @@ def test_param_type_help_metavar(parameter: Any, expected_metavar: str) -> None:
     app = typer.Typer()
 
     @app.command()
-    # TODO: type-specific default
     def with_default(value: parameter = "my_default"):
         pass  # pragma: no cover
 
