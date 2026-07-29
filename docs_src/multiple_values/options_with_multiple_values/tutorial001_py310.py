eb0d6cc2c8d4b3f53b04136782a729fec5c229c7
svlandeg <sofie.vanlandeghem@gmail.com>
2026-07-29 18:26:52 +0200

    update tutorial to have None as default for a tuple

--- docs_src/multiple_values/options_with_multiple_values/tutorial001_py310.py
+++ docs_src/multiple_values/options_with_multiple_values/tutorial001_py310.py
@@ -4,11 +4,11 @@ app = typer.Typer()
 
 
 @app.command()
-def main(user: tuple[str, int, bool] = typer.Option((None, None, None))):
-    username, coins, is_wizard = user
-    if not username:
+def main(user: tuple[str, int, bool] | None = typer.Option(None)):
+    if not user:
         print("No user provided")
         raise typer.Abort()
+    username, coins, is_wizard = user
     print(f"The username {username} has {coins} coins")
     if is_wizard:
         print("And this user is a wizard!")
